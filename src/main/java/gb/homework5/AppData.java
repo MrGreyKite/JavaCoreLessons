1e3e20e7bc9061dd7ba5161d1faa6f7091a0f630
Рудольф Багмет <nolde3@yandex.ru>
2022-02-02 21:32:19 +0400

    homework 5 - read&write

--- src/main/java/gb/homework5/AppData.java
+++ src/main/java/gb/homework5/AppData.java
@@ -9,12 +9,13 @@ public class AppData {
     private List<String> headers = new ArrayList<>();
     private List<List<Integer>> records = new ArrayList<>();
 
+    String delimiter = ";";
+
     public void read(File file) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             int rows;
 
-            String delimiter = ";";
             String[] headersArray = reader.readLine().split(delimiter);
             rows = headersArray.length;
             for (String header : headersArray) {
@@ -35,8 +36,34 @@ public class AppData {
             e.printStackTrace();
         }
 
-        System.out.println(headers);
-        System.out.println(records);
+    }
+
+    public List<String> getHeaders() {
+        return headers;
+    }
+
+    public List<List<Integer>> getRecords() {
+        return records;
+    }
+
+    public void write(File file) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+
+            for (String header : headers) {
+                writer.write(header + delimiter);
+            }
+            writer.newLine();
+
+            for (List<Integer> recordLine : records) {
+                for (Integer record : recordLine) {
+                    writer.write(record + delimiter);
+                }
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/gb/homework5/Main.java
+++ src/main/java/gb/homework5/Main.java
@@ -8,6 +8,11 @@ public class Main {
         File file = new File("src/main/resources/1.csv");
         AppData data = new AppData();
         data.read(file);
+        System.out.println(data.getHeaders());
+        System.out.println(data.getRecords());
+
+        File fileTo = new File("src/main/resources/2.csv");
+        data.write(fileTo);
 
     }
 }
